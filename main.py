20424ca265c8568548f4b11d9788b3b9ea5b6feb
alexandretoullec <69418272+alexandretoullec@users.noreply.github.com>
2022-06-29 17:41:50 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
 
 window = Tk()
 window.config(padx=50, pady=50)
@@ -7,13 +10,52 @@ window.title("Password Manager")
 
 #functions
 
+def generate():
+    password_input.delete(0,END)
+    password = ""
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    letters_list = [random.choice(letters) for _ in range(nr_letters)]
+    symbols_list = [random.choice(symbols) for _ in range(nr_symbols)]
+    number_list = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = letters_list + symbols_list + number_list
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    # for char in password_list:
+    #     password += char
+
+    password_input.insert(0,password)
+
+
+
 def save():
     website_save = website_input.get()
     email_save = email_input.get()
     password_save = password_input.get()
-    f = open("data_files", "a")
-    f.write(f"{website_save} | {email_save} | {password_save},\n")
-    f.close()
+
+    if website_save == "" or password_save == "":
+        messagebox.showinfo(title="warning", message="You shouldn't let any field empty !!!")
+
+    else:
+        #message box
+        is_ok = messagebox.askokcancel(title="website", message=f"These are the details entered: \nEmail: {email_save}\nPassword: {password_save}\nIs it ok for save?")
+
+        if is_ok:
+            with open("data_files", "a") as data_file:
+                data_file.write(f"{website_save} | {email_save} | {password_save},\n")
+            website_input.delete(0,"end")
+            password_input.delete(0,"end")
 
 #Image
 canvas = Canvas(width=200, height=200)
@@ -49,7 +91,7 @@ password_input.grid(column=1, row=3, sticky="W")
 
 
 #Buttons
-generate_button = Button(text="Generate")
+generate_button = Button(text="Generate", command=generate)
 generate_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=42, command=save)
